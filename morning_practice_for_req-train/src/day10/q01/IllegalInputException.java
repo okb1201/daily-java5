8c939e5f37975bd7f0052c0ddb15d73e6b0b6cc0
user <user@WIN-EQ8BHT0D8BB>
2025-11-28 17:36:13 +0900

    朝の練習問題 Day10

---
+++ morning_practice_for_req-train/src/day10/q01/IllegalInputException.java
@@ -0,0 +1,12 @@
+package day10.q01;
+
+public class IllegalInputException extends Exception {
+
+	public IllegalInputException() {
+	}
+
+	public IllegalInputException(String message) {
+		super(message);
+	}
+
+}
--- morning_practice_for_req-train/src/day10/q01/Main.java
+++ morning_practice_for_req-train/src/day10/q01/Main.java
@@ -64,6 +64,24 @@ package day10.q01;
 
 public class Main {
 
-	// ここから追記してください
+	public static void main(String[] args) {
+
+		Account suzuki = new Account();
+		ConsoleReader consoleReader = new ConsoleReader();
+		int pin = 0;
+		try {
+			pin = consoleReader.inputPIN();
+			if (suzuki.getPin() == pin) {
+				System.out.println("認証しました");
+				suzuki.showAccount();
+			} else {
+				System.out.println("暗証番号が違います。");
+			}
+		} catch (IllegalInputException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
+
